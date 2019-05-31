c858c02df692cc3bcdd940e4164e8096a720d9da
BaseMax <MaxBaseCode@Gmail.Com>
2019-05-31 18:13:48 +0430

    Apply some new change and find a bug...

--- ls.c
+++ ls.c
@@ -1,16 +1,49 @@
 #include "ls.h"
 
-void ls() {
-	const char *directionPath = directionPath = getenv("PWD");
+// pwd
+unsigned char pwd(char *directionPath) {
+	directionPath = getenv("PWD");
+	// printf("==>%s\n", directionPath);
 	if(directionPath == NULL) {
+		// printf("Error: Could not get the working directory!\n");
+		// return;
+		return 0;
+	}
+	return 1;
+}
+
+// readDir
+unsigned char readDir(char** directionPath, DIR *directionCurrent) {
+	// printf("==>%s\n", directionPath);
+	directionCurrent = opendir(*directionPath);
+	if(directionCurrent == NULL) {
+		// printf("Error: Could not open the working directory!\n");
+		// return;
+		return 0;
+	}
+	return 1;
+}
+
+// ls
+void ls() {
+	char *directionPath;
+	unsigned char result;
+
+	result = pwd(directionPath);
+	if(result == 0) {
 		printf("Error: Could not get the working directory!\n");
 		return;
 	}
-	DIR *directionCurrent  = opendir(directionPath);
-	if(directionCurrent == NULL) {
+
+	printf("==>%s\n", directionPath);
+
+	DIR *directionCurrent;
+	result = readDir(&directionPath, directionCurrent);
+	if(result == 0) {
 		printf("Error: Could not open the working directory!\n");
 		return;
 	}
+
 	size_t count = 0;
 	struct dirent *item = NULL;
 	for(count = 0;(item = readdir(directionCurrent)) != NULL;count++) { 
